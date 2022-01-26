8abe214a3308721905b184745fbb70c9331f5d99
DELL <DELL@Sibasankar>
2022-01-26 22:33:31 +0530

    first one

---
+++ src/test/java/com/crm/ComCast/ContactTest/CreateContactTest.java
@@ -0,0 +1,24 @@
+package com.crm.ComCast.ContactTest;
+
+import org.testng.annotations.Test;
+
+
+public class CreateContactTest {
+
+	@Test(groups="smokeTest")
+	public static void createContactTest() 
+	{
+
+    	System.out.println("execte createcontact");
+    	
+
+		}
+	 @Test(groups="regressionTest")
+	 public void createContactwithOrgTest()
+	 {
+		 System.out.println("this is contact with org ");
+	 }
+
+	}
+
+
---
+++ src/test/java/com/crm/ComCast/OrgTest/CreateOrgTest.java
@@ -0,0 +1,23 @@
+package com.crm.ComCast.OrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+    @Test(groups="smokeTest")
+	public  void createOrgTest() 
+	{
+    	
+    	System.out.println("execte createorg");
+    
+    
+    	
+    
+
+	}
+  @Test (groups="regressionTest") 
+ public void createOrgwithIndustryTest()
+ {
+	 System.out.println("this is org with industry ");
+ }
+
+}
